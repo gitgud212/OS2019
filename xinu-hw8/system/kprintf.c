80a12808068d9c2057c606ae71f725e6ae6fea62
kaickh <khasukai@gmail.com>
2019-04-04 17:56:33 -0500

    the newest tarball

---
+++ xinu-hw8/system/kprintf.c
@@ -0,0 +1,128 @@
+/**
+ *TA-BOT:MAILTO khasukai@gmail.com charles.titzer@marquette.edu
+ * @file kprintf.c
+ */
+
+/* Embedded Xinu, Copyright (C) 2009, 2013.  All rights reserved. */
+
+#include <xinu.h>
+
+#define UNGETMAX 10             /* Can un-get at most 10 characters. */
+int counter = 0;
+static unsigned char ungetArray[UNGETMAX];
+
+/**
+ * Synchronously read a character from a UART.  This blocks until a character is
+ * available.  The interrupt handler is not used.
+ *
+ * @return
+ *      The character read from the UART as an <code>unsigned char</code> cast
+ *      to an <code>int</code>.
+ */
+syscall kgetc(void)
+{
+    volatile struct pl011_uart_csreg *regptr;
+
+    /* Pointer to the UART control and status registers.  */
+    regptr = (struct pl011_uart_csreg *)0x3F201000;
+	
+    // TODO: First, check the unget buffer for a character.
+    //       Otherwise, check UART flags register, and
+    //       once the receiver is not empty, get character c.
+	if (counter>0){
+		counter--;
+		return ungetArray[counter];
+	}
+	while(regptr -> fr & PL011_FR_RXFE) {}
+
+	return ((int)regptr -> dr);
+
+    return SYSERR;
+}
+
+/**
+ * kcheckc - check to see if a character is available.
+ * @return true if a character is available, false otherwise.
+ */
+syscall kcheckc(void)
+{
+    volatile struct pl011_uart_csreg *regptr;
+    regptr = (struct pl011_uart_csreg *)0x3F201000;
+
+    // TODO: Check the unget buffer and the UART for characters
+	
+    if (regptr -> fr & PL011_FR_RXFE && counter==0)	//changed this bit, should work now.
+	return 0;			
+    else 
+	return 1;		
+
+    return SYSERR;
+}
+
+/**
+ * kungetc - put a serial character "back" into a local buffer.
+ * @param c character to unget.
+ * @return c on success, SYSERR on failure.
+ */
+syscall kungetc(unsigned char c)
+{
+    // TODO: Check for room in unget buffer, put the character in or discard.
+    if (counter < UNGETMAX - 1){ 	//if there is room in the unget buffer, count will be 0-9 		
+	ungetArray[counter] = c;	//c will be placed in unget array at index counter. counter is incremented 
+	counter++;			//and c is returned
+    	return c;
+    }
+		
+    return SYSERR;
+}
+
+
+/**
+ * Synchronously write a character to a UART.  This blocks until the character
+ * has been written to the hardware.  The interrupt handler is not used.
+ *
+ * @param c
+ *      The character to write.
+ *
+ * @return
+ *      The character written to the UART as an <code>unsigned char</code> cast
+ *      to an <code>int</code>.
+ */
+syscall kputc(uchar c)
+{
+    volatile struct pl011_uart_csreg *regptr;
+
+    /* Pointer to the UART control and status registers.  */
+    regptr = (struct pl011_uart_csreg *)0x3F201000;
+
+    // TODO: Check UART flags register.
+    //       Once the Transmitter FIFO is not full, send character c.
+    while (regptr->fr & PL011_FR_TXFF) {}
+	
+	regptr->dr =c;
+    return SYSERR;
+}
+
+/**
+ * kernel printf: formatted, synchronous output to SERIAL0.
+ *
+ * @param format
+ *      The format string.  Not all standard format specifiers are supported by
+ *      this implementation.  See _doprnt() for a description of supported
+ *      conversion specifications.
+ * @param ...
+ *      Arguments matching those in the format string.
+ *
+ * @return
+ *      The number of characters written.
+ */
+syscall kprintf(const char *format, ...)
+{
+    int retval;
+    va_list ap;
+
+    va_start(ap, format);
+    retval = _doprnt(format, ap, (int (*)(int, int))kputc, 0);
+    va_end(ap);
+    return retval;
+}
